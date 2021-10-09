de54a28d22632a81408f41a1e82fe30e78fc5b21
tinhangchui <tinhangc@usc.edu>
2021-10-08 17:30:49 -0700

    modified robo_gym and added wrappers

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# BeoSim robo_gym
+<img align="left" width="60" height="60" src="https://user-images.githubusercontent.com/36470989/116858354-8668f380-abfe-11eb-81dc-629d9e8a9d4e.png" alt="robo-gym logo">
+
+<!-- omit in toc -->
+# robo-gym 
+
+<br>
+
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/robo-gym)
+![PyPI](https://img.shields.io/pypi/v/robo-gym)
+
+This is a repo forked from the original [robo-gym](https://github.com/jr-robotics/robo-gym) by jr-robotics. I added a customized robot model and few environments on top of robo-gym for our lab.
+
+# Installation
+1. Install [modified robo-gym-robot-servers](https://github.com/tinhangchui/robo-gym-robot-servers).
+
+2. Install [modified robo-gym-server-modules](https://github.com/tinhangchui/robo-gym-server-modules).
+
+3. Finally, run the following commands:
+```bash
+git clone https://github.com/tinhangchui/robo-gym.git
+cd robo-gym
+pip install -e .
+```
---
+++ robo_gym/__init__.py
@@ -0,0 +1,98 @@
+from gym.envs.registration import register
+
+# naming convention: EnvnameRobotSim
+
+# Example Environments
+register(
+    id='ExampleEnvSim-v0',
+    entry_point='robo_gym.envs:ExampleEnvSim',
+)
+
+register(
+    id='ExampleEnvRob-v0',
+    entry_point='robo_gym.envs:ExampleEnvRob',
+)
+
+# MiR100 Environments
+register(
+    id='NoObstacleNavigationMir100Sim-v0',
+    entry_point='robo_gym.envs:NoObstacleNavigationMir100Sim',
+)
+
+register(
+    id='NoObstacleNavigationMir100Rob-v0',
+    entry_point='robo_gym.envs:NoObstacleNavigationMir100Rob',
+)
+
+register(
+    id='ObstacleAvoidanceMir100Sim-v0',
+    entry_point='robo_gym.envs:ObstacleAvoidanceMir100Sim',
+)
+
+register(
+    id='ObstacleAvoidanceMir100Rob-v0',
+    entry_point='robo_gym.envs:ObstacleAvoidanceMir100Rob',
+)
+
+# UR Environments
+register(
+    id='EmptyEnvironmentURSim-v0',
+    entry_point='robo_gym.envs:EmptyEnvironmentURSim',
+)
+
+register(
+    id='EmptyEnvironmentURRob-v0',
+    entry_point='robo_gym.envs:EmptyEnvironmentURRob',
+)
+
+register(
+    id='EndEffectorPositioningURSim-v0',
+    entry_point='robo_gym.envs:EndEffectorPositioningURSim',
+)
+
+register(
+    id='EndEffectorPositioningURRob-v0',
+    entry_point='robo_gym.envs:EndEffectorPositioningURRob',
+)
+
+register(
+    id='BasicAvoidanceURSim-v0',
+    entry_point='robo_gym.envs:BasicAvoidanceURSim',
+)
+
+register(
+    id='BasicAvoidanceURRob-v0',
+    entry_point='robo_gym.envs:BasicAvoidanceURRob',
+)
+
+register(
+    id='AvoidanceIros2021URSim-v0',
+    entry_point='robo_gym.envs:AvoidanceIros2021URSim',
+)
+
+register(
+    id='AvoidanceIros2021URRob-v0',
+    entry_point='robo_gym.envs:AvoidanceIros2021URRob',
+)
+
+register(
+    id='AvoidanceIros2021TestURSim-v0',
+    entry_point='robo_gym.envs:AvoidanceIros2021TestURSim',
+)
+
+register(
+    id='AvoidanceIros2021TestURRob-v0',
+    entry_point='robo_gym.envs:AvoidanceIros2021TestURRob',
+)
+
+register(
+    id='WayPointSim-v0',
+    entry_point='robo_gym.envs:WayPointSim',
+)
+
+# Beosim environment
+register(
+    id='BeoarmEEPosition-v0',
+    entry_point='robo_gym.envs:BeoarmEEPosition',
+)
+
